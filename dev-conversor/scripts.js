e87b17c3c66ded34084e056227653230869fd0c9
telmochrist <telmochrist@yahoo.com.br>
2026-02-27 22:35:29 -0300

    novos testes

--- dev-conversor/scripts.js
+++ dev-conversor/scripts.js
@@ -1,4 +1,29 @@
 
-const covertButton = document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button")
 
-console.log(convertButton)
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value 
+    const valorToConvert = document.querySelector(".valor-to-convert")
+    const valorConverted = document.querySelector(".valor")
+
+    const dolarToday = 5.5
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    valorToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    valorConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
+    
+ 
+
+
+}
+
+convertButton.addEventListener("click", convertValues)
